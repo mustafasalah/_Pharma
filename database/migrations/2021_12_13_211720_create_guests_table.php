acc0b1d65331a908123f8cdd56809ca7c1da7a8f
Mohammed <mhmdtajeldin@gmail.com>
2021-12-21 08:12:33 +0200

    Added the guest.

---
+++ database/migrations/2021_12_13_211720_create_guests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone_number',14);
+            $table->foreignId('address_id')->constrained('addresses')->onUpdate('cascade');
+            $table->timestamp('last_seen')->useCurrent();
+            $table->timestamp('create_time')->useCurrent();
+            $table->foreignId('address_id')->constrained('addresses')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
